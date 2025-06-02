54db89d01ea520e5a3c39e5f89d9b356cef7b603
Владислав Будин <ladudin121@mail.ru>
2025-06-02 16:04:16 +0300

    name_n with check

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ def print_name3():
 
 def print_name4():
     print("ЕГОР")
+
+n = int(input())
+if n not in (1, 2, 3, 4):
+    raise ValueError("Номер должен быть от 1 до 4")
+if n == 1:
+    print_name1()
+elif n == 2:
+    print_name2()
+elif n == 3:
+    print_name3()
+elif n == 4:
+    print_name4()
